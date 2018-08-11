466fd8f68ed42a059e13a255263fe76cba1e3938
Richard Le <richardle1997@gmail.com>
2018-08-11 16:57:04 -0700

    Copied assets from node_modules to their own asset folders

--- js/testScene.js
+++ js/testScene.js
@@ -1,3 +1,3 @@
-particlesJS.load('background', 'node_modules/particle.js/demo/particles.json', function() {
+particlesJS.load('background', 'js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
