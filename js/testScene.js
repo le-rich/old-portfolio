3cf5b2e3827b62483d4c6bb6e279307eb6a6ac60
Richard Le <richardle1997@gmail.com>
2018-08-11 16:54:04 -0700

    Removed Threejs, found a more lightweight library that has a more stylistic particle system

--- js/testScene.js
+++ js/testScene.js
@@ -1,31 +1,3 @@
-window.addEventListener( 'resize', onWindowResize, false );
-
-var backgroundElement = document.getElementById("background");
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, $(backgroundElement).width() / $(backgroundElement).height(), 0.1, 10000 );
-var renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-
-backgroundElement.appendChild( renderer.domElement );
-renderer.setSize($(backgroundElement).width(),$(backgroundElement).height());
-renderer.autoClear = false;
-renderer.setClearColor(0x000000, 0.0);
-camera.position.z = 100;
-
-var particleSystem = new THREE.GPUParticleSystem({
-	maxParticles: 250000
+particlesJS.load('background', 'node_modules/particle.js/demo/particles.json', function() {
+  console.log('callback - particles.js config loaded');
 });
-
-
-function animate(){
-	requestAnimationFrame(animate);
-	renderer.render( scene, camera);	
-}
-
-function onWindowResize(){
-    camera.aspect = $(backgroundElement).width() / $(backgroundElement).height();
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( $(backgroundElement).width() , $(backgroundElement).height() );
-}
-
-animate();
