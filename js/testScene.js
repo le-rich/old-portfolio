0eb002161e0207b3641fe04c3660082e7a8cefcb
Richard Le <richardle1997@gmail.com>
2018-08-12 21:04:02 -0700

    Removed functionlity for seriousljs, won't give effects desired.

--- js/testScene.js
+++ js/testScene.js
@@ -1,27 +1,44 @@
+window.addEventListener( 'resize', onWindowResize, false );
 
-init();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer({alpha: true});
 
+renderer.setClearColor(0x000000, 0.0)
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.domElement.id = 'threeCanvas';
+var backgroundDiv = document.querySelector('#background');
+backgroundDiv.appendChild( renderer.domElement );
 
-function init(){
-	var canvas = document.querySelector('#effectCanvas');
-	var particleCanvas = document.querySelector('.particles-js-canvas-el');
-	canvas.getContext('webgl');
-	canvas.height = particleCanvas.height;
-	canvas.width = particleCanvas.width;
-	console.log(canvas.getContext('webgl'));
+var geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-	var seriously = new Seriously();
-	var src = seriously.source('.particles-js-canvas-el')
-	var target = seriously.target('#effectCanvas');
-	var grain = seriously.effect('filmgrain');
-	grain.amount = 1;
-	var vignette = seriously.effect('vignette');
-	grain.source = src;
-	vignette.source = grain;
-	target.source = vignette;
+camera.position.z = 5;
 
-	seriously.go();
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
 }
+
+animate();
+
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
 
 
 
