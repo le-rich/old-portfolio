7e5bf9c03afe05e1a29d00a9463efeb2a50f9ca3
Richard Le <richardle1997@gmail.com>
2018-08-27 11:59:47 -0700

    Added lighting to scene

--- js/testScene.js
+++ js/testScene.js
@@ -2,7 +2,7 @@ window.addEventListener( 'resize', onWindowResize, false );
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer({alpha: true});
+var renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
 
 renderer.setClearColor(0x000000, 0.0)
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,8 +11,22 @@ var backgroundDiv = document.querySelector('#background');
 backgroundDiv.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshStandardMaterial( { color: 0xff0051 } );
 var cube = new THREE.Mesh( geometry, material );
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+var pointLight = new THREE.PointLight(0xffffff, 1);
+
+
+var icoGeo = new THREE.IcosahedronGeometry(3, 1);
+var icoMaterial = new THREE.MeshBasicMaterial({ color: 0x606060, wireframe: true, transparent: true});
+var wireFrameIco = new THREE.Mesh(icoGeo, icoMaterial);
+
+
+pointLight.position.set(25, 50, 25);
+
+scene.add(wireFrameIco);
+scene.add(pointLight);
+scene.add(ambientLight);
 scene.add( cube );
 
 camera.position.z = 5;
@@ -22,6 +36,9 @@ var animate = function () {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+	wireFrameIco.rotation.x -= 0.001;
+	wireFrameIco.rotation.y -= 0.001;
 
 	renderer.render( scene, camera );
 }
