5185b2c4f3c8d28ac4f3f720f475389da5785dfb
Richard Le <richardle1997@gmail.com>
2018-08-31 01:50:24 -0700

    Made title more responsive, moved geometry below title

--- js/testScene.js
+++ js/testScene.js
@@ -20,7 +20,6 @@ mtlLoader.load('assets/editedHead.mtl', function(materials){
 
 		object.rotation.y = 90;
 		object.position.x = 0;
-		object.position.y = -1.2;
 		object.scale.x = 0.4;
 		object.scale.y = 0.4;
 		object.scale.z = 0.4;
@@ -92,7 +91,7 @@ var animate = function () {
 
 	if (myModels[0] != null){
 		myModels[0].rotation.y += 0.01;
-		myModels[0].position.y = Math.sin(clock.getElapsedTime()) * 0.2;
+		myModels[0].position.y = Math.sin(clock.getElapsedTime()) * 0.2; - 0.3;
 	}
 	
 	wireFrameIco.rotation.x -= 0.001;
