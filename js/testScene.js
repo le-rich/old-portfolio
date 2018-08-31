4c590310ba72beaaf92f514e9060f10edc682313
Richard Le <richardle1997@gmail.com>
2018-08-31 03:08:34 -0700

    Made title more responsive, moved geometry below title

--- js/testScene.js
+++ js/testScene.js
@@ -1,5 +1,5 @@
 window.addEventListener( 'resize', onWindowResize, false );
-document.onmousemove = lookatMouse;
+document.onmousemove = lookAtMouse;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
